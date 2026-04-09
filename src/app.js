d46ea90ef6376a3d6f45a69d1954887b6674cd6d
SapphicBear <sleepyscreenart@gmail.com>
2026-04-09 21:57:03 +0200

    Set up app.js and pool.js

--- src/app.js
+++ src/app.js
@@ -1,7 +1,20 @@
 const express = require("express");
 const path = require("node:path");
+
 const PORT = 3000;
 const app = express();
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
+
+
+app.use((error, req, res, next) => {
+    console.error(error);
+    res.status(error.statusCode || 500).send(error.message);
+});
 
 app.listen(PORT, (err) => {
     if (err) {
