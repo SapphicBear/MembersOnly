d04cf629db58d02d509c78742d7454c852282d48
SapphicBear <sleepyscreenart@gmail.com>
2026-04-10 15:05:32 +0200

    Create links and indexController

---
+++ src/controllers/indexController.js
@@ -0,0 +1,13 @@
+import links from "../data/links";
+
+async function indexGet(req, res) {
+    const header = "Secret Message";
+    const title = "Members Only!";
+    res.render("index", 
+        {
+            title: title,
+            header: header,
+            links: links,
+        });
+}
+export { indexGet };
