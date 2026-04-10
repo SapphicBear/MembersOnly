afea84c27000ff57938456ecdd0f0c239f7ba5f1
SapphicBear <sleepyscreenart@gmail.com>
2026-04-10 18:20:48 +0200

    lint fix

--- src/routes/signUpRoute.js
+++ src/routes/signUpRoute.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import * as signUp from "./../controllers/signUpController.js";
 
 const router = Router();
+
 router.get("/", signUp.getSignUp);
 
 export { router as signUpRouter };
