a3c7cbbd66ce20ae63829e1122fa9e71fb591534
SapphicBear <sleepyscreenart@gmail.com>
2026-04-11 16:12:44 +0200

    Change expiration date for cookies

--- src/app.js
+++ src/app.js
@@ -30,7 +30,7 @@ app.use(session({
     store: new pgSession({
         pool: pool
     }),
-    cookie: { maxAge: 30 * 24 * 60 * 60 * 1000 }
+    cookie: { maxAge: 60000 * 60 * 2 }
 }));
 app.use(passport.session());
 
