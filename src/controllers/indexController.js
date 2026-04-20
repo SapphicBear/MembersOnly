730bd667a0be03340b1e36691d2e0fa231bf9f9c
SapphicBear <sleepyscreenart@gmail.com>
2026-04-20 18:10:01 +0200

    Change time to show date after 5 days
    Make the timestamp show a time stamp until 5 days, then showing
    the date information and timestamp of when the message was posted.

--- src/controllers/indexController.js
+++ src/controllers/indexController.js
@@ -1,13 +1,20 @@
 import { links } from "../data/links.js";
 import { titles } from "../data/titles.js";
 import * as db from "../../db/queries.js";
-import { formatDistanceToNow } from "date-fns";
+import { formatDistanceToNow, format } from "date-fns";
 
 async function indexGet(req, res) {
     const header = "Secret Message";
     const messages = await db.getMessages();
     messages.map((message) => {
-        message.date = formatDistanceToNow(message.date, { includeSeconds: true });
+        const now = new Date().toISOString().slice(0, 10).split("-");
+        const date = message.date.slice(0, 10).split("-");
+        
+        if (date[1] !== now[1] || (now[2] - date[2]) > 5) {
+            message.date = format(message.date, "PPP - HH:mm z");
+        } else {
+            message.date = formatDistanceToNow(message.date, { includeSeconds: true }) + " ago";
+        }
     });
     res.render("index", 
         {
